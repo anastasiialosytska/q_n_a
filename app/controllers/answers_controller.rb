38f8dc785a7deafb9ef7bc065cf71c36bc45fa2f
Anastasiia.Losytska <alosytska@gmail.com>
2022-06-24 13:36:01 +0300

    Creating answer with attaced files

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -27,11 +27,11 @@ class AnswersController < ApplicationController
   end
 
   def set_answer
-    @answer = Answer.find(params[:id])
+    @answer = Answer.with_attached_files.find(params[:id])
   end
 
   def answer_params
-    params.require(:answer).permit(:body)
+    params.require(:answer).permit(:body, files: [])
   end
 
   def get_list_of_answers
